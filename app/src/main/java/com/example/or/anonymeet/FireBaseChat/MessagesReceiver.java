c9f81356b462b66c4064fa6f17586bf2e50a48e5
oriyair1 <oriyair1@gmail.com>
2016-03-30 16:27:23 +0300

    123

--- app/src/main/java/com/example/or/anonymeet/FireBaseChat/MessagesReceiver.java
+++ app/src/main/java/com/example/or/anonymeet/FireBaseChat/MessagesReceiver.java
@@ -2,9 +2,12 @@ package com.example.or.anonymeet.FireBaseChat;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.example.or.anonymeet.R;
@@ -23,13 +26,20 @@ public class MessagesReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("hiiiiiiiiiiiiiiiiii", "Receiver started");
-        Notification n  = new Notification.Builder(context)
+        Notification.Builder n  = new Notification.Builder(context)
                 .setContentTitle("New mail from " + "test@gmail.com")
                 .setContentText("Subject")
-                .setSmallIcon(R.drawable.ic_media_play)
+                .setSmallIcon(R.drawable.contact)
                 .setAutoCancel(true)
-                .build();
+                .setTicker("hiiiiii")
+                .setDefaults(NotificationCompat.DEFAULT_SOUND);
+        TaskStackBuilder t = TaskStackBuilder.create(context);
+        Intent i = new Intent(context, MessagesActivity.class);
+        t.addParentStack(MessagesActivity.class);
+        t.addNextIntent(i);
+        PendingIntent pendingIntent = t.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        n.setContentIntent(pendingIntent);
+        nm.notify(0, n.build());
 
-        nm.notify(0, n);
     }
 }
